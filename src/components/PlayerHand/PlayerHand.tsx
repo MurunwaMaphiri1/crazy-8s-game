aae4f05e97f4491bd7fbf49e7077b1c204c1a4e7
Murunwa <134610001+MurunwaMaphiri1@users.noreply.github.com>
2025-11-09 23:02:28 +0200

    Changes to the bot avatar path and Minor UI changes in PlayerHand

--- src/components/PlayerHand/PlayerHand.tsx
+++ src/components/PlayerHand/PlayerHand.tsx
@@ -13,7 +13,7 @@ export default function PlayerHand({ cards, onCardClick }: PlayerHandProps) {
             <div className="flex justify-center gap-1 mt-4">
                 {cards.map((card, i) => (
                     <div
-                        key={crypto.randomUUID()}
+                        key={card.code}
                         className="sm:ml-0 -ml-15 first:ml-0 md:ml:0 -ml-15 first:ml-0"
                         style={{zIndex: i}}
                     >
